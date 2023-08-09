2eec3ae98696616393897cbdbed4c1af10732bd6
Jean Pierre Ndagijimana <jeanpierrendagije0@gmail.com>
2023-08-09 14:44:01 +0200

    sedders next

--- seeders/20230726091806-assignments.cjs
+++ seeders/20230726091806-assignments.cjs
@@ -7,25 +7,25 @@ module.exports = {
       {
         createdAt: new Date(),
         updatedAt: new Date(),
-        devices_id: 1,
+        devices_id: 13,
         employees_id: 1,
       },
       {
         createdAt: new Date(),
         updatedAt: new Date(),
-        devices_id: 1,
+        devices_id: 14,
         employees_id: 2,
       },
       {
         createdAt: new Date(),
         updatedAt: new Date(),
-        devices_id: 3,
+        devices_id: 15,
         employees_id: 2,
       },
       {
         createdAt: new Date(),
         updatedAt: new Date(),
-        devices_id: 4,
+        devices_id: 16,
         employees_id: 3,
       },
     ]);
